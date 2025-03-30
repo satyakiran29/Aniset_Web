2fdab91a99ddaa4c1ff2d53fa38cdf355fdbf83b
Satyakiran <87798342+satyakiran29@users.noreply.github.com>
2025-03-30 21:39:23 +0530

    Loaders Added Ringloader From https://www.davidhu.io/react-spinners/

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { RingLoader } from 'react-spinners';  // Importing HashLoader
 
 import Navbar from './components/Pages/Navbar';
 import Footer from './components/Pages/footer';
@@ -10,26 +11,51 @@ import Pricing from './components/Pages/paid';
 import Gallery from './components/Pages/cpage/Gallery';
 import './components/Pages/cpage/gallery.css'
 import PrivacyPolicy from './components/Pages/cpage/PrivacyPolicy/PrivacyPolicy';
+
 const App = () => {
+  const [loading, setLoading] = useState(true);  // Use state to track loading status
+
+  useEffect(() => {
+    // Simulate a loading state for 3 seconds (replace this with your actual loading logic)
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
   return (
-    <Router>
-      <div className="app">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={
-            <>
-              <Hero />
-              <StatsSection />
-              <Pricing />
-              <Community />
-            </>
-          } />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path='/privacypolicy' element={<PrivacyPolicy />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+    <>
+      {loading ? (
+        <div className="loader" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+          <RingLoader
+            color={'white'}
+            loading={loading}
+            size={100}
+            cssOverride={{ display: 'block', margin: '0 auto' }}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>
+      ) : (
+        <Router>
+          <div className="app">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={
+                <>
+                  <Hero />
+                  <StatsSection />
+                  <Pricing />
+                  <Community />
+                </>
+              } />
+              <Route path="/gallery" element={<Gallery />} />
+              <Route path='/privacypolicy' element={<PrivacyPolicy />} />
+            </Routes>
+            <Footer />
+          </div>
+        </Router>
+      )}
+    </>
   );
 };
 
